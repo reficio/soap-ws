4aecd0e3cf79ebd7e238df3e5d18942a230546ad
Tom Bujok <tom.bujok@reficio.org>
2012-11-22 17:21:11 +0100

    Adding simple values provider

--- soap-common/src/main/java/com/centeractive/ws/SoapMultiValuesProvider.java
+++ soap-common/src/main/java/com/centeractive/ws/SoapMultiValuesProvider.java
@@ -21,6 +21,10 @@ package com.centeractive.ws;
 import javax.xml.namespace.QName;
 import java.util.Set;
 
+/**
+ * @author Tom Bujok
+ * @since 1.0.0
+ */
 public interface SoapMultiValuesProvider {
 
     Set<String> getMultiValues(QName name);
