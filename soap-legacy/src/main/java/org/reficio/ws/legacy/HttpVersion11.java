6468b84e39c74c9d116a13387cee43103c4e739f
Venkat Ramana Lashkar <ramana.kathek@gmail.com>
2017-03-06 23:17:15 -0600

    Added soapversion to utils for version check

---
+++ soap-legacy/src/main/java/org/reficio/ws/legacy/HttpVersion11.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2012-2013 Reficio (TM) - Reestablish your software!. All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package org.reficio.ws.legacy;
+
+import org.apache.xmlbeans.SchemaType;
+import org.apache.xmlbeans.SchemaTypeLoader;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+import org.xmlsoap.schemas.soap.envelope.EnvelopeDocument;
+
+import javax.xml.namespace.QName;
+import java.io.IOException;
+
+/**
+ * This class was extracted from the soapUI code base by centeractive ag in October 2011.
+ * The main reason behind the extraction was to separate the code that is responsible
+ * for the generation of the SOAP messages from the rest of the soapUI's code that is
+ * tightly coupled with other modules, such as soapUI's graphical user interface, etc.
+ * The goal was to create an open-source java project whose main responsibility is to
+ * handle SOAP message generation and SOAP transmission purely on an XML level.
+ * <br/>
+ * centeractive ag would like to express strong appreciation to SmartBear Software and
+ * to the whole team of soapUI's developers for creating soapUI and for releasing its
+ * source code under a free and open-source licence. centeractive ag extracted and
+ * modifies some parts of the soapUI's code in good faith, making every effort not
+ * to impair any existing functionality and to supplement it according to our
+ * requirements, applying best practices of software design.
+ *
+ * Changes done:
+ * - changing location in the package structure
+ * - removal of dependencies and code parts that are out of scope of SOAP message generation
+ * - minor fixes to make the class compile out of soapUI's code base
+ * - changing the mechanism using which external resources are loaded
+ */
+
+/**
+ * SoapVersion for Http 1.1
+ *
+ * @author ramana
+ */
+class HttpVersion11 extends AbstractSoapVersion {
+    private final static QName envelopeQName = new QName(Constants.SOAP11_ENVELOPE_NS, "");
+    private final static QName bodyQName = new QName(Constants.SOAP11_ENVELOPE_NS, "Body");
+    private final static QName faultQName = new QName(Constants.SOAP11_ENVELOPE_NS, "Fault");
+    private final static QName headerQName = new QName(Constants.SOAP11_ENVELOPE_NS, "Header");
+
+    SchemaTypeLoader soapSchema;
+    SchemaType soapEnvelopeType;
+    private XmlObject soapSchemaXml;
+    private XmlObject soapEncodingXml;
+    private SchemaType soapFaultType;
+
+    public final static HttpVersion11 instance = new HttpVersion11();
+
+    private HttpVersion11() {
+    }
+
+    public SchemaType getEnvelopeType() {
+        return EnvelopeDocument.type;
+    }
+
+    public String getEnvelopeNamespace() {
+        return Constants.SOAP11_ENVELOPE_NS;
+    }
+
+    public String getEncodingNamespace() {
+        return Constants.SOAP_ENCODING_NS;
+    }
+
+    public XmlObject getSoapEncodingSchema() throws XmlException, IOException {
+        return soapEncodingXml;
+    }
+
+    public XmlObject getSoapEnvelopeSchema() throws XmlException, IOException {
+        return soapSchemaXml;
+    }
+
+    public String toString() {
+        return "Http 1.1";
+    }
+
+    public String getContentTypeHttpHeader(String encoding, String soapAction) {
+        if (encoding == null || encoding.trim().length() == 0)
+            return getContentType();
+        else
+            return getContentType() + ";charset=" + encoding;
+    }
+
+    public String getSoapActionHeader(String soapAction) {
+        return null;
+    }
+
+    public String getContentType() {
+        return "text/xml";
+    }
+
+    public QName getBodyQName() {
+        return bodyQName;
+    }
+
+    public QName getEnvelopeQName() {
+        return envelopeQName;
+    }
+
+    public QName getHeaderQName() {
+        return headerQName;
+    }
+
+    protected SchemaTypeLoader getSoapEnvelopeSchemaLoader() {
+        return soapSchema;
+    }
+
+    public SchemaType getFaultType() {
+        return soapFaultType;
+    }
+
+    public String getName() {
+        return "Http 1.1";
+    }
+
+    public String getFaultDetailNamespace() {
+        return "";
+    }
+}
